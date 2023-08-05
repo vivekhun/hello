735f919a51d2228cfd8286b96b5bcc0a004d1045
Vivekanand <vivek.hunsimarad@gmail.com>
2023-08-05 13:40:59 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        int c=a+b;
+        System.out.println(c);
+        System.out.println("Hello and welcome!");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
