22bfa4a66290cfdc9812dfafab53a716e6c4e839
Vivekanand <vivek.hunsimarad@gmail.com>
2023-08-05 13:45:02 +0530

    first commit removed c

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         int a=5;
         int b=6;
-
+int c=5;
         System.out.println(c);
         System.out.println("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
